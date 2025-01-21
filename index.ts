9a6c1d4741106f68dd7e37c323f73b3aefa9b9d8
Mustafaadeel1 <mustafaadeel989@gmail.com>
2025-01-21 23:03:00 +0500

    my first code

--- index.ts
+++ index.ts
@@ -1,11 +1,19 @@
+// This is my First code of TypeScript :)
+// let is type of variable declaration in TypeScript
 let hello = "Hello World";
 console.log(hello);
 
-
+// var is the another example type of variable declaration in TypeScript
 var name1 = "world";
 
 function sayHello() {
     console.log(name1);
 }
- const hello1 = "Hello World";
- console.log(hello);
+
+// const is the another example type of variable declaration in TypeScript
+
+const hello1 = "Hello World";
+console.log(hello);
+
+// In my fist code of typescript i declare a variable with let, var and const keyword and print the value of variable in console.
+// I also create a function and call it to consol(print) the value of variable.
